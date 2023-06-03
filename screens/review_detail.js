231dc273f826b3a5c8a4287ca83d780f8303ded1
nomenklatur <dimasekaputra2000@gmail.com>
2023-06-03 13:38:44 +0700

    (feat) add rating images

--- screens/review_detail.js
+++ screens/review_detail.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetail({ navigation, route }) {
-  const { body, title, rating } = route.params
+  const { body, title, rating } = route.params;
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text style={globalStyles.titleText}>{ title }</Text>
-        <Text>Rating: { rating }/5</Text>
         <Text>" { body } "</Text>
+        <View style={styles.rating}>
+          <Text>Game Rating: </Text>
+          <Image source={images.ratings[rating]}/>
+        </View>
       </Card>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee'
+  }
+});
--- styles/global.js
+++ styles/global.js
@@ -14,3 +14,13 @@ export const globalStyles = StyleSheet.create({
     lineHeight: 20
   }
 });
+
+export const images = {
+  ratings: {
+    '1': require('../assets/images/rating-1.png'),
+    '2': require('../assets/images/rating-2.png'),
+    '3': require('../assets/images/rating-3.png'),
+    '4': require('../assets/images/rating-4.png'),
+    '5': require('../assets/images/rating-5.png'),
+  }
+};
